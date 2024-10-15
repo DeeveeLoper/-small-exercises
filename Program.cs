79349b6a235455200843da4596ea618c4ceab7be
RobCam <roberto.camacho4@gmail.com>
2024-10-15 18:01:39 +0200

    Små övningen 2.2 är tillagd i koden

--- Program.cs
+++ Program.cs
@@ -3,12 +3,15 @@
     static void Main(string[] args)
     {
 
-        new sumNumer().displaySumNumber();
+        //new SumNumer().displaySumNumber();
+
+        new AverageValue().displayAverageValue();
+
 
     }
 
     // 2.1 Skapa ett program där användaren får mata in två heltal, varefter programmet beräknar summan.
-    class sumNumer
+    class SumNumer
     {
         public void displaySumNumber()
         {
@@ -22,5 +25,27 @@
             Console.WriteLine($"Summan blir: {sumNumbers}");
         }
     }
+
+    // 2.2 Skapa ett program där användaren får mata in tre heltal, varefter programmet beräknar summan och medelvärdet av dessa tal.
+    class AverageValue
+    {
+        public void displayAverageValue()
+        {
+            Console.Write("Skriv in först heltalet: ");
+            int firstNumber = int.Parse(Console.ReadLine());
+
+            Console.Write("Skriv in först heltalet: ");
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            Console.Write("Skriv in först heltalet: ");
+            int thirdNumber = int.Parse(Console.ReadLine());
+
+            int sumNumbers = firstNumber + secondNumber + thirdNumber;
+            int averageValue = sumNumbers / 3;
+
+            Console.WriteLine($"Summan är {sumNumbers} och medelvärdet är {averageValue}.");
+        }
+    }
+
 
 }
