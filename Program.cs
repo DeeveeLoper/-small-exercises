5b6687cd4fda0e7eaf854a818ef29901a42a9b65
RobCam <roberto.camacho4@gmail.com>
2024-10-15 17:40:18 +0200

    Added a new project

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿class SmallExercises
+{
+    static void Main(string[] args)
+    {
+
+        new sumNumer().displaySumNumber();
+
+    }
+
+    // 2.1 Skapa ett program där användaren får mata in två heltal, varefter programmet beräknar summan.
+    class sumNumer
+    {
+        public void displaySumNumber()
+        {
+            Console.Write("Skriv första helttalet: ");
+            int firstNumber = int.Parse(Console.ReadLine());
+
+            Console.Write("Skriv andra helttalet: ");
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            int sumNumbers = firstNumber + secondNumber;
+            Console.WriteLine($"Summan blir: {sumNumbers}");
+        }
+    }
+
+}
