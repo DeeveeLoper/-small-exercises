cccaa2ac44739111550bca2cebe42e09b0654661
RobCam <roberto.camacho4@gmail.com>
2024-10-15 18:28:12 +0200

    Small exercise 2.4 is added to the code

--- Program.cs
+++ Program.cs
@@ -7,7 +7,10 @@
 
         //new AverageValue().displayAverageValue();
 
-        new StoryWithTwoNames().displayStoryWithTwoNames();
+        //new StoryWithTwoNames().displayStoryWithTwoNames();
+
+        new floatingNumber().displayFloatingNumber();
+
 
 
 
@@ -50,7 +53,7 @@
         }
     }
 
-    // Låt användaren mata in namnet på två personer. Programmet ska sedan skriva ut en kort berättelse om dessa två personer.
+    // 2.3 Låt användaren mata in namnet på två personer. Programmet ska sedan skriva ut en kort berättelse om dessa två personer.
     class StoryWithTwoNames
     {
         public void displayStoryWithTwoNames()
@@ -64,6 +67,20 @@
             Console.WriteLine($"Det var en gång en prins som heter {firstName} och han gifte sig med en prinsessa som heter {secondName}. Och dem levde lyckliga i alla sina dar. ");
         }
     }
+
+    // 2.4 Använd metoden Math.Round() för att avrunda ett flyttal som användaren skriver in till två decimaler. 
+    // Om användaren t.ex. skriver in t.ex. 2.56789 så ska programmet avrunade det till 2.57
+    class floatingNumber
+    {
+        public void displayFloatingNumber()
+        {
+            Console.WriteLine("Skriv in ett flyttal som ska avrundas:");
+            float floatingNumber = float.Parse(Console.ReadLine());
+            double roundedNumber = Math.Round(floatingNumber, 2);
+            Console.WriteLine($"Avrundat flyttal är: {roundedNumber}");
+        }
+    }
+
 
 
 }
